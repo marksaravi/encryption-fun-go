4b612a6b2517f8602d3be0fa759d1415e176dcf7
marksaravi <mark.saravi@gmail.com>
2023-05-06 10:09:04 +1000

    1000000 primes

--- cmd/phi.go
+++ cmd/phi.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	primenumbers "github.com/marksaravi/encryption-fun-go/prime-numbers"
-)
-
 func main() {
-	for i := 1; i < 2; i++ {
-		fmt.Println(primenumbers.Phi(i))
-	}
 }
--- rsa/rsa.go
+++ rsa/rsa.go
@@ -1,7 +1,30 @@
 package rsa
 
-// func generateKeys() {
-// 	p1, p2 := primenumbers.GetPrimes()
-// 	n := int64(p1) * int64(p2)
-// 	return
-// }
+import primenumbers "github.com/marksaravi/encryption-fun-go/prime-numbers"
+
+func generateKeys() {
+	// p, q := primenumbers.GetPrimes()
+	// n, e := primenumbers.NE(p, q)
+
+	// return
+}
+
+func genPQ() (int, int) {
+	p, q := primenumbers.GetPrimes()
+
+	return p, q
+}
+
+func genPublicKey(p, q int) (int, int) {
+	n, e := primenumbers.NE(p, q)
+
+	return n, e
+}
+
+func genPrivateKey(p, q, e int) (int, int) {
+	// phi := (p - 1) * (q - 1)
+	// x := float64(phi+1) / float64(e)
+	// // if x<1
+	// return n, e
+	return 0, 0
+}
