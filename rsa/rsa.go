f5000f0c196960111af43ac6d49d85c57933fa10
marksaravi <mark.saravi@gmail.com>
2023-05-06 10:30:25 +1000

    remnaming

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -2,29 +2,10 @@ package rsa
 
 import primenumbers "github.com/marksaravi/encryption-fun-go/prime-numbers"
 
-func generateKeys() {
-	// p, q := primenumbers.GetPrimes()
+func generateKeys() int64 {
+	P, Q := primenumbers.GetPrimesPQ()
+	var N int64 = P * Q
 	// n, e := primenumbers.NE(p, q)
 
-	// return
-}
-
-func genPQ() (int, int) {
-	p, q := primenumbers.GetPrimes()
-
-	return p, q
-}
-
-func genPublicKey(p, q int) (int, int) {
-	n, e := primenumbers.NE(p, q)
-
-	return n, e
-}
-
-func genPrivateKey(p, q, e int) (int, int) {
-	// phi := (p - 1) * (q - 1)
-	// x := float64(phi+1) / float64(e)
-	// // if x<1
-	// return n, e
-	return 0, 0
+	return N
 }
