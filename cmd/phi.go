e3021334e4ae531b3d02ccd9155685e67ca15b84
marksaravi <mark.saravi@gmail.com>
2023-05-06 11:15:02 +1000

    find coprimes

--- cmd/phi.go
+++ cmd/phi.go
@@ -7,10 +7,9 @@ import (
 )
 
 func main() {
-	fmt.Printf("4   digits: %d\n", len(primenumbers.PrimeNumbers4Digits))
-	// fmt.Printf("5   digits: %d\n", len(primenumbers.PrimeNumbers5Digits))
-	// fmt.Printf("6   digits: %d\n", len(primenumbers.PrimeNumbers6Digits))
-	// fmt.Printf("7   digits: %d\n", len(primenumbers.PrimeNumbers7Digits))
-	// fmt.Printf("8   digits: %d\n", len(primenumbers.PrimeNumbers8Digits))
-	// fmt.Printf("all digit: %d\n", len(primenumbers.PrimeNumbersMillion))
+	for i := 1; i <= 36; i++ {
+		coprimes := primenumbers.FindCoprimes(i)
+		// fmt.Printf("%d: %d (%v)\n", i, len(coprimes), coprimes)
+		fmt.Printf("%2d: %2d\n", i, len(coprimes))
+	}
 }
--- prime-numbers/prime-numbers.go
+++ prime-numbers/prime-numbers.go
@@ -14,20 +14,30 @@ func init() {
 }
 
 func GetPrimesPQ() (P int64, Q int64) {
-	i1 := randomGenerator.Intn(len(PrimeNumbers4Digits))
-	i2 := randomGenerator.Intn(len(PrimeNumbers4Digits))
+	i1 := randomGenerator.Intn(len(PrimeNumbers) / 2)
+	i2 := randomGenerator.Intn(len(PrimeNumbers) / 2)
 	for i1 == i2 {
-		i2 = randomGenerator.Intn(len(PrimeNumbers4Digits))
+		i2 = randomGenerator.Intn(len(PrimeNumbers))
 	}
-	P = int64(PrimeNumbers4Digits[i1])
-	Q = int64(PrimeNumbers4Digits[i2])
+	P = int64(PrimeNumbers[i1])
+	Q = int64(PrimeNumbers[i2])
 	return
 }
 
 func FindCoprimes(a int) []int {
 	coprimes := make([]int, 0, 1000)
+	coprimes = append(coprimes, 1)
 	for n := 2; n < a; n++ {
-
+		iscoprime := true
+		for p := 0; PrimeNumbers[p] <= n; p++ {
+			if a%PrimeNumbers[p] == 0 && n%PrimeNumbers[p] == 0 {
+				iscoprime = false
+				break
+			}
+		}
+		if iscoprime {
+			coprimes = append(coprimes, n)
+		}
 	}
 	return coprimes
 }
