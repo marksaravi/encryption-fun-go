707440d690b38922c184eb6ec564cce910748295
marksaravi <mark.saravi@gmail.com>
2023-07-03 21:49:13 +1000

    to int64

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -1,20 +1,22 @@
 package rsa
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/marksaravi/encryption-fun-go/mathematics"
+	primenumbers "github.com/marksaravi/encryption-fun-go/prime-numbers"
 )
 
-func GenerateKeys() (n, publicKey, PrivateKey int) {
-	// P, Q := primenumbers.GetPrimesPQ()
-	P := 61
-	Q := 53
-	var N int = P * Q
+func GenerateKeys() (n, publicKey, PrivateKey int64) {
+	P, Q := primenumbers.GetPrimesPQ()
+	// P := 61
+	// Q := 53
+	var N int64 = P * Q
 	carmichael := mathematics.CarmichaelOfPQ(P, Q)
 
-	fmt.Println(carmichael)
-	e := 17
+	coPrimes := mathematics.FindCoprimes(carmichael)
+	i := time.Now().Nanosecond() % len(coPrimes)
+	e := coPrimes[i]
 	modularMultiplicativeInverse, _, _, _ := mathematics.ModularMultiplicativeInverseGCD1(17, carmichael)
 
 	return N, e, modularMultiplicativeInverse
